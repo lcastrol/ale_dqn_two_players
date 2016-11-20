e58e48e7e503b1a06f528650575cdf8f47e97f9d
lcastrol101 <lcastrol101@tu-berlin.de>
2016-11-20 21:26:00 +0100

    Adding plotter script and new plotting features, best fitting PDF

---
+++ plot.sh
@@ -0,0 +1,1 @@
+./plots.py --histogram --destiny /home/lcastrol/Documents/KTH/Research/work/ale_dqn_two_players/plot /home/lcastrol/Documents/KTH/Research/work/ale_dqn_two_players/boxing_1479657129.csv
--- plots.py
+++ plots.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python2
 
-import csv
 import os
+import csv
 import numpy as np
+import scipy
+import scipy.stats as st
+import pandas as pd
 import matplotlib.pyplot as plt
+import statsmodels as sm
 import matplotlib.patches as patches
 import matplotlib.path as path
-
 import argparse
 
 def plot():
@@ -53,39 +56,124 @@ def plot():
 
 def plot_histogram(data, destiny):
 
-    fig, ax = plt.subplots()
 
-    # histogram our data with numpy
-    n, bins = np.histogram(data, 50)
+    # Plot histogram
+    plt.hist(data, bins=range(min(data),max(data)), color='g', normed=True)
 
-    # get the corners of the rectangles for the histogram
-    left = np.array(bins[:-1])
-    right = np.array(bins[1:])
-    bottom = np.zeros(len(left))
-    top = bottom + n
+    ##Fit a normal distribution
+    best_fit_dist_name, best_fit_params = best_fit_distribution(data, bins=range(min(data),max(data)))
+    #dist = getattr(st, 'norm')
+    dist = getattr(st, best_fit_dist_name)
+    #param = dist.fit(data)
+    #param = best_fit_params
 
 
-    # we need a (numrects x numsides x 2) numpy array for the path helper
-    # function to build a compound path
-    XY = np.array([[left, left, right, right], [bottom, top, top, bottom]]).T
+    # Make PDF
+    pdf = make_pdf(dist, best_fit_params)
+    pdf.plot(lw=2, label='PDF', legend=True)
 
-    # get the Path object
-    barpath = path.Path.make_compound_path_from_polys(XY)
+    #For a normal distribution the keyword parameter loc defines the mean and the keyword parameter scale defines the standard deviation.
+    #pdf_fitted = dist.pdf(scipy.arange(-len(data)/2,len(data)/2), *param[:-2], loc=param[-2], scale=param[-1]) * len(data)
+    plt.xlim(-20,20) #TODO fix this, to be smart
+    #plt.plot(scipy.arange(-len(data)/2,len(data)/2), pdf_fitted, 'r-', label='normal dist')
 
-    # make a patch out of it
-    patch = patches.PathPatch(
-        barpath, facecolor='blue', edgecolor='gray', alpha=0.8)
-    ax.add_patch(patch)
+    param_names = (dist.shapes + ', loc, scale').split(', ') if dist.shapes else ['loc', 'scale']
+    param_str = ', '.join(['{}={:0.2f}'.format(k,v) for k,v in zip(param_names, best_fit_params)])
+    dist_str = '{}'.format(param_str)
 
-    # update the view limits
-    ax.set_xlim(left[0], right[-1])
-    ax.set_ylim(bottom.min(), top.max())
+    mean, variance = dist.stats(*best_fit_params[:-2],moments='mv')
+    median = dist.median(*best_fit_params[:-2],loc=best_fit_params[-2],scale=best_fit_params[-1])
 
-    plt.title('Game score histogram')
+    plt.xlabel('Final score difference (Player A - Player B)')
+    plt.title(('Game score histogram \n Best fitting distribution: %s \n Mean = %.2f, Variance = %.2f, Median = %.2f \n' % (best_fit_dist_name, mean,variance,median)) + dist_str)
     plt.grid(True)
     plt.savefig("%s/game_score_histogram.png" % destiny)
     plt.close()
-    #plt.show()
+
+# Create models from data
+def best_fit_distribution(data, bins=200, ax=None):
+    """Model data by finding best fit distribution to data"""
+    # Get histogram of original data
+    y, x = np.histogram(data, bins=bins, normed=True)
+    x = (x + np.roll(x, -1))[:-1] / 2.0
+
+    # Distributions to check
+    DISTRIBUTIONS = [
+        st.alpha,st.anglit,st.arcsine,st.beta,st.betaprime,st.bradford,st.burr,st.cauchy,st.chi,st.chi2,st.cosine,
+        st.dgamma,st.dweibull,st.erlang,st.expon,st.exponnorm,st.exponweib,st.exponpow,st.f,st.fatiguelife,st.fisk,
+        st.foldcauchy,st.foldnorm,st.frechet_r,st.frechet_l,st.genlogistic,st.genpareto,st.gennorm,st.genexpon,
+        st.genextreme,st.gausshyper,st.gamma,st.gengamma,st.genhalflogistic,st.gilbrat,st.gompertz,st.gumbel_r,
+        st.gumbel_l,st.halfcauchy,st.halflogistic,st.halfnorm,st.halfgennorm,st.hypsecant,st.invgamma,st.invgauss,
+        st.invweibull,st.johnsonsb,st.johnsonsu,st.ksone,st.kstwobign,st.laplace,st.levy,st.levy_l,st.levy_stable,
+        st.logistic,st.loggamma,st.loglaplace,st.lognorm,st.lomax,st.maxwell,st.mielke,st.nakagami,st.ncx2,st.ncf,
+        st.nct,st.norm,st.pareto,st.pearson3,st.powerlaw,st.powerlognorm,st.powernorm,st.rdist,st.reciprocal,
+        st.rayleigh,st.rice,st.recipinvgauss,st.semicircular,st.t,st.triang,st.truncexpon,st.truncnorm,st.tukeylambda,
+        st.uniform,st.vonmises,st.vonmises_line,st.wald,st.weibull_min,st.weibull_max,st.wrapcauchy
+    ]
+
+    # Best holders
+    best_distribution = st.norm
+    best_params = (0.0, 1.0)
+    best_sse = np.inf
+
+    # Estimate distribution parameters from data
+    for distribution in DISTRIBUTIONS:
+
+        # Try to fit the distribution
+        try:
+            # Ignore warnings from data that can't be fit with warnings.catch_warnings():
+                #warnings.filterwarnings('ignore')
+
+                # fit dist to data
+                params = distribution.fit(data)
+
+                # Separate parts of parameters
+                arg = params[:-2]
+                loc = params[-2]
+                scale = params[-1]
+
+                # Calculate fitted PDF and error with fit in distribution
+                pdf = distribution.pdf(x, loc=loc, scale=scale, *arg)
+                sse = np.sum(np.power(y - pdf, 2.0))
+
+                # if axis pass in add to plot
+                try:
+                    if ax:
+                        pd.Series(pdf, x).plot(ax=ax)
+                    end
+                except Exception:
+                    pass
+
+                # identify if this distribution is better
+                if best_sse > sse > 0:
+                    best_distribution = distribution
+                    best_params = params
+                    best_sse = sse
+
+        except Exception:
+            print 'error out'
+            pass
+
+    return (best_distribution.name, best_params)
+
+def make_pdf(dist, params, size=10000):
+    """Generate distributions's Propbability Distribution Function """
+
+    # Separate parts of parameters
+    arg = params[:-2]
+    loc = params[-2]
+    scale = params[-1]
+
+    # Get sane start and end points of distribution
+    start = dist.ppf(0.01, *arg, loc=loc, scale=scale) if arg else dist.ppf(0.01, loc=loc, scale=scale)
+    end = dist.ppf(0.99, *arg, loc=loc, scale=scale) if arg else dist.ppf(0.99, loc=loc, scale=scale)
+
+    # Build PDF and turn into pandas Series
+    x = np.linspace(start, end, size)
+    y = dist.pdf(x, loc=loc, scale=scale, *arg)
+    pdf = pd.Series(y, x)
+
+    return pdf
 
 # plot average difference / frame skip opponent graphs with fixed frame skip of frame_skip
 def plot_frame_skip(x_list, y_list, destiny):
