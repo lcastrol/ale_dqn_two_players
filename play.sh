3e5d60389d8a975674514e3655658bb0154de922
Jens R <rowekamp@kth.se>
2017-01-10 18:43:22 +0100

    data collection finished

--- play.sh
+++ play.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 #A shell script to execute the ALE-Boxing experiments
 
-./main.py --no-screen-display --saved-model-dir=exp_8_3 --log-dir-name=exp_8_3 --device=cpu --handle=play --play_epsilon=0.8 --iterations=30 --nn-file=/home/luis/ALE_Boxing/ale_dqn_two_players/saved_networks_1479689039/boxing_sarsa/boxing_200.pkl --model_file=/home/luis/ALE_Boxing/ale_dqn_two_players/saved_networks_1479689039/boxing_dqn/boxing-dqn-143534100 boxing &
-./main.py --no-screen-display --saved-model-dir=exp_8_4 --log-dir-name=exp_8_5 --device=cpu --handle=play --play_epsilon=0.8 --iterations=30 --nn-file=/home/luis/ALE_Boxing/ale_dqn_two_players/saved_networks_1479689039/boxing_sarsa/boxing_200.pkl --model_file=/home/luis/ALE_Boxing/ale_dqn_two_players/saved_networks_1479689039/boxing_dqn/boxing-dqn-143534100 boxing & 
+nohup ./main.py --no-screen-display --saved-model-dir=exp_0_1 --log-dir-name=exp_0_1 --device=cpu --handle=play --play_epsilon=0.2 --iterations=100 --nn-file=/home/luis/ALE_Boxing/ale_dqn_two_players_testing/saved_networks_1481058035/boxing_sarsa/boxing_1201.pkl --model_file=/home/luis/ALE_Boxing/ale_dqn_two_players_testing/saved_networks_1481058035/boxing_dqn/boxing-dqn-1083478686 boxing > exp_0_1.out 2> exp_0_1.err &
+
+nohup ./main.py --no-screen-display --saved-model-dir=exp_0_2 --log-dir-name=exp_0_2 --device=cpu --handle=play --play_epsilon=0.2 --iterations=100 --nn-file=/home/luis/ALE_Boxing/ale_dqn_two_players_testing/saved_networks_1481058035/boxing_sarsa/boxing_1201.pkl --model_file=/home/luis/ALE_Boxing/ale_dqn_two_players_testing/saved_networks_1481058035/boxing_dqn/boxing-dqn-1083478686 boxing > exp_0_2.out 2> exp_0_2.err &
+
+nohup ./main.py --no-screen-display --saved-model-dir=exp_0_3 --log-dir-name=exp_0_3 --device=cpu --handle=play --play_epsilon=0.2 --iterations=100 --nn-file=/home/luis/ALE_Boxing/ale_dqn_two_players_testing/saved_networks_1481058035/boxing_sarsa/boxing_1201.pkl --model_file=/home/luis/ALE_Boxing/ale_dqn_two_players_testing/saved_networks_1481058035/boxing_dqn/boxing-dqn-1083478686 boxing > exp_0_3.out 2> exp_0_3.err &
+
